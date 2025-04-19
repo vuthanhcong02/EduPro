ba30965abfb149214f8cf08418a8fe6155c2269d
Vu Thanh Cong <congvtc02@gmail.com>
2025-04-19 15:15:55 +0700

    Add component stats pro project

--- fe-app/src/layouts/DefaultLayout.jsx
+++ fe-app/src/layouts/DefaultLayout.jsx
@@ -1,14 +1,12 @@
 import Header from "../components/Layouts/Header";
 import Footer from "../components/Layouts/Footer";
 import { Outlet } from "react-router-dom";
-import Banner from "../components/Layouts/Banner";
 
 export default function DefaultLayout() {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Header />
 			<main className="flex-1">
-				<Banner />
 				<Outlet />
 			</main>
 			<Footer />
--- fe-app/src/pages/Home.jsx
+++ fe-app/src/pages/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import Banner from "../components/Layouts/Banner";
+import Stats from "../components/Layouts/Stats";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<>
+			<Banner />
+			<Stats />
+			<div className="text-center">Home</div>
+		</>
+	);
 }
